54649cf84f529c2f2674fdd4171b985c37ece7e0
Lellalu <lella.lu14@outlook.com>
2024-09-06 18:24:03 +0200

    update task 5

--- task5.py
+++ task5.py
@@ -1,7 +1,4 @@
 import numpy as np
-from numpy import random
-import matplotlib.pyplot as plt
-import pandas as pd
 import scipy
 
 def calc_prob_list(n):
@@ -13,7 +10,7 @@ def calc_prob_list(n):
 
 real_prob = 0.010677083333333334
 
-for i in range(11000, 20000):
+for i in range(11600, 20000):
   prob_list = calc_prob_list(i)
   most_probable_prob = np.argmax(prob_list)/i
   err_value = abs(most_probable_prob-real_prob)/real_prob
